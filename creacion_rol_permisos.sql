9a8cc97b846683a5ab57119542f29a43a401aec2
Manuel Retamozo <manuel.retamozo@gmail.com>
2017-01-05 13:01:34 -0300

    Update creacion_rol_permisos.sql
    Permiso de actualizacion

--- creacion_rol_permisos.sql
+++ creacion_rol_permisos.sql
@@ -7,7 +7,7 @@ COMMENT ON ROLE tecnicos_regional
   IS 'Tecnico Regional, Editor colaborativo auditado, propone cambios en tablas de contribuciones. (aportes)';
 */
 -- Permisos de lectura de indice y tabla. Permiso de escritura a tabla plantaciones aportes.
-GRANT SELECT, INSERT ON TABLE plantaciones TO GROUP tecnicos_regional;
+GRANT SELECT, INSERT, UPDATE ON TABLE plantaciones TO tecnicos_regional;
 
 GRANT USAGE, SELECT, UPDATE  ON TABLE public.plantaciones_id_seq TO GROUP tecnicos_regional;
 
